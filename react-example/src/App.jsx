f4064d6849c63c5383acea44e4770a79bcb0c139
Estefani <yuditheylb0@gmail.com>
2024-04-14 19:50:13 -0400

    feature/Q-3

--- react-example/src/App.jsx
+++ react-example/src/App.jsx
@@ -1,4 +1,3 @@
-
 import './App.css'
 import { Button } from './components/Button';
 import { List } from './components/List'
--- react-example/src/components/Button.jsx
+++ react-example/src/components/Button.jsx
@@ -1,17 +1,18 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 export const Button = props => {
-
     const { text } = props;
-
-    const[count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
     function handlerButton() {
+        const randomNumber = Math.floor(Math.random() * 100) + 1;
+        alert(`Número aleatorio: ${randomNumber}`);
         setCount(count + 5);
     }
+
     return (
-        <button onClick={ handlerButton }>
-            { text } { count }
+        <button onClick={handlerButton}>
+            {text} {count}
         </button>
     );
-}
+};
